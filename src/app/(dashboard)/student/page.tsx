c8deb84d5823a1d01eb5a50da8823f887672f071
Ujwal <ujwalkhatiwada501@gmail.com>
2025-09-30 00:09:57 +0545

    fixed some bugs

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,5 @@
 import Announcements from "@/components/Announcements";
 import BigCalendarContainer from "@/components/BigCalendarContainer";
-import BigCalendar from "@/components/BigCalender";
 import EventCalendar from "@/components/EventCalendar";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
@@ -14,16 +13,24 @@ const StudentPage = async () => {
     },
   });
 
-  console.log(classItem);
+  const classId = classItem.length > 0 ? classItem[0].id : undefined;
+
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-          <BigCalendarContainer type="classId" id={classItem[0].id} />
+          <h1 className="text-xl font-semibold">
+            Schedule {classItem.length > 0 ? `(4A)` : ""}
+          </h1>
+          {classId ? (
+            <BigCalendarContainer type="classId" id={classId} />
+          ) : (
+            <p className="text-gray-500 mt-4">No class assigned yet.</p>
+          )}
         </div>
       </div>
+
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <EventCalendar />
